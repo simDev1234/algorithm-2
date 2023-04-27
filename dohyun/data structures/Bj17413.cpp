2c046ecb1c961ebd3d4f9dfb1bc424aa210296ae
destitutor <binarybard@pm.me>
2023-04-27 22:11:32 +0900

    refactor: 들여쓰기 변경

--- dohyun/data structures/Bj17413.cpp
+++ dohyun/data structures/Bj17413.cpp
@@ -3,14 +3,14 @@
 using namespace std;
 
 int main() {
-    string s;
+	string s;
 	getline(cin, s);
 
 	regex r("(<[^>]*>| )");
-    for (sregex_token_iterator i(all(s), r, {-1, 0}), e; i != e; ++i) {
-        string t = *i;
+	for (sregex_token_iterator i(all(s), r, {-1, 0}), e; i != e; ++i) {
+		string t = *i;
 		if (t[0] != '<')
 			reverse(all(t));
 		cout << t;
-    }
+	}
 }
