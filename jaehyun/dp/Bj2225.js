9e443ccda73608eef30fb9c283e348e3847130ab
Jaehyeon <jaehyeonp74@gmail.com>
2023-05-04 23:44:46 +0900

    2225 다른 풀이

--- jaehyun/dp/Bj2225.js
+++ jaehyun/dp/Bj2225.js
@@ -8,8 +8,10 @@ const [N, K] = input.split(" ").map(Number);
 // 예를 들어 합이 4가 되는 경우 맨 뒷자리에 들어올 숫자를 고정시키고, 들어올 수 있는 경우의 수의 합
 // 맨 뒤가 0이면 합이 4가 되기 위해 4만 들어올 수 있음
 
+const dp = [];
+
 for (let i = 1; i <= K; i++) {
-  dp[i] = new Array(n + 1).fill(i === 1 ? 1 : 0);
+  dp[i] = new Array(N + 1).fill(i === 1 ? 1 : 0);
   dp[i][0] = 1;
 }
 
@@ -21,4 +23,4 @@ for (let i = 2; i <= K; i++) {
   }
 }
 
-console.log(dp[k][n]);
+console.log(dp[K][N]);
