ffced40d0b869b315503b87b0f0ed6d426c8e680
Jaehyeon <jaehyeonp74@gmail.com>
2023-06-03 01:19:45 +0900

    12904 다른 풀이

--- jaehyun/string/Bj12904.js
+++ jaehyun/string/Bj12904.js
@@ -3,31 +3,27 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "example.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
 const [S, T] = input;
+let deque = input[1].split("");
+let dir = "left";
 
-let answer = 0;
-
-function bfs() {
-  const q = [[S, false]];
-  while (q.length) {
-    let [str, hasTurned] = q.shift();
-    if (str.length === T.length) {
-      if (hasTurned) str = str.split("").reverse("").join("");
-      if (str === T) {
-        answer = 1;
-        break;
-      }
-      continue;
+for (let i = 0; i < T.length - S.length; i++) {
+  if (dir === "left") {
+    let check = deque.pop();
+    if (check === "B") {
+      dir = "right";
     }
-    if (str.length > T.length) continue;
-    if (!hasTurned) {
-      q.push([str + "A", hasTurned]);
-      q.push(["B" + str, !hasTurned]);
-    } else {
-      q.push(["A" + str, hasTurned]);
-      q.push([str + "B", !hasTurned]);
+  } else {
+    let check = deque.shift();
+    if (check === "B") {
+      dir = "left";
     }
   }
 }
-
-bfs();
-console.log(answer);
+if (dir === "right") {
+  deque.reverse();
+}
+if (deque.join("") === S) {
+  console.log(1);
+} else {
+  console.log(0);
+}
